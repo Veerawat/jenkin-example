12dab5996e117bf6acbfeb823719c4150a08ad4d
wat <veerawat74@gmail.com>
2018-11-25 19:54:38 +0700

    init file

---
+++ HelloWorld.go
@@ -0,0 +1,13 @@
+// public class HelloWorld {
+//     public static main(String[] args) {
+//         System.out.print("HelloWorld");
+//     }
+// }
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello world")
+}
